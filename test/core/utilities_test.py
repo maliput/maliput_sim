029dfe14b0b51265347c896b19f5de191fb20f5a
Franco Cipollone <53065142+francocipollone@users.noreply.github.com>
2023-07-14 08:19:16 -0300

    Adds tests for IDProvider class (#27)
    Signed-off-by: Franco Cipollone <franco.c@ekumenlabs.com>

---
+++ test/core/utilities_test.py
@@ -0,0 +1,48 @@
+# BSD 3-Clause License
+#
+# Copyright (c) 2023, Woven by Toyota
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import unittest
+from maliput_sim.core.utilities import IDProvider
+
+
+class IDProviderTestCase(unittest.TestCase):
+    def test_new_id(self):
+        # Create an instance of IDProvider
+        id_provider = IDProvider()
+
+        # Call the new_id() method twice
+        id1 = id_provider.new_id()
+        id2 = id_provider.new_id()
+
+        # Assert that the IDs are not empty
+        self.assertNotEqual(id1, '')
+        self.assertNotEqual(id2, '')
+
+        # Assert that the IDs are different
+        self.assertNotEqual(id1, id2)
--- test/dummy_test.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-
-# Dummy unitest
-class DummyTest(unittest.TestCase):
-    def test_dummy(self):
-        self.assertTrue(True)
